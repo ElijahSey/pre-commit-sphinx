e2fabf4f8257a55130d2434b2063c39d60f66c4c
Seyfarth, Elijah <seyfarthelijah2002@gmail.com>
2024-05-02 23:46:28 +0200

    Add hook script

---
+++ hook.sh
@@ -0,0 +1,12 @@
+sphinx-apidoc $@
+
+tracked=$(git diff --name-only *.rst)
+untracked=$(git ls-files --other --exclude-standard)
+
+if [ -z "$tracked" ]; then
+    if [ -z "$untracked" ]; then
+        exit 0
+    fi
+fi
+
+exit 1
