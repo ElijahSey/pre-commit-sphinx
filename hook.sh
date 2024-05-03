2deb2dad2c6eb69506a8ce5153eb8ba1670d946c
Seyfarth, Elijah <seyfarthelijah2002@gmail.com>
2024-05-03 12:16:41 +0200

    Include untracked files in checking

--- hook.sh
+++ hook.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
+
 prediff=$(git diff *.rst)
+prefiles=$(git ls-files --other --exclude-standard)
+
 sphinx-apidoc $@
+
 postdiff=$(git diff *.rst)
+postfiles=$(git ls-files --other --exclude-standard)
 
 if [ "$prediff" = "$postdiff" ]; then
-    exit 0
+    if [ "$prefiles" = "$postfiles"]; then
+        exit 0
+    fi
 fi
 
 exit 1
