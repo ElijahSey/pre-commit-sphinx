68deb96a8c2a25a92205596fc5b8bc7369dbf0b4
Seyfarth, Elijah <seyfarthelijah2002@gmail.com>
2024-05-04 13:53:45 +0200

    Integrate diff check in git status command

--- sphinx_pre_commit.py
+++ sphinx_pre_commit.py
@@ -3,28 +3,20 @@ import subprocess
 import sys
 
 
-def get_git_diff():
-    return subprocess.run(
-        ["git", "diff", "*.rst"], capture_output=True, text=True
-    ).stdout
-
-
 def get_git_status():
     return subprocess.run(
-        ["git", "status", "-s"], capture_output=True, text=True
+        ["git", "status", "-s", "-v", "*.rst"], capture_output=True, text=True
     ).stdout
 
 
 def main():
-    prediff = get_git_diff()
     prestatus = get_git_status()
 
     subprocess.run(["sphinx-apidoc"] + sys.argv[1:])
 
-    postdiff = get_git_diff()
     poststatus = get_git_status()
 
-    if prediff == postdiff and prestatus == poststatus:
+    if prestatus == poststatus:
         sys.exit(0)
     else:
         sys.exit(1)
